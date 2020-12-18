47de489716bc89dfcb9696bc9c059fb807c54e5a
mcgillij <mcgillivray.jason@gmail.com>
2020-12-18 14:08:19 -0400

    adding mitch's changes

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import os
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
@@ -13,15 +15,19 @@ async def on_message(message):
         return
 
     if str(message.author) in ["robbbot#6138"]:
-      await message.add_reaction('🟪')
-    
-    if str(message.author) in ["Desert Ham#2846"]:
-      await message.add_reaction('<:DS:784463585531265104>')
+        await message.add_reaction('🟪')
+
+    if str(message.author) in ["barftron666#9511"]:
+        await message.add_reaction('👐')    if str(message.author) in ["SinSemilla#3965"]:
+        await message.add_reaction('<:NH:784463585531265104>'
+      if str(message.author) in ["Desert Ham#2846"]:
+        await message.add_reaction('<:DS:784463585531265104>')
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-    if message.content.startswith('$darcysux' ) or message.content.startswith('$ds'):
+    if message.content.startswith('$darcysux') or message.content.startswith(
+            '$ds'):
         await message.channel.send('<:DS:784463585531265104>')
 
     if message.content.startswith('$thumb'):
@@ -32,14 +38,16 @@ async def on_message(message):
             return user == message.author and str(reaction.emoji) == '👍'
 
         try:
-            reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
+            reaction, user = await client.wait_for(
+                'reaction_add', timeout=60.0, check=check)
         except asyncio.TimeoutError:
             await channel.send('👎')
         else:
-            await channel.send('👍')  
-            
+            await channel.send('👍')
     if message.content.startswith('$commands'):
         channel = message.channel
-        await channel.send('$hello, $ds, $darcysux, $commands')       
+        await channel.send('$hello, $ds, $darcysux, $commands')
+
 
 client.run(os.getenv('TOKEN'))
+
